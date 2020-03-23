52efbbf2b2bf9af368fb8efb06096bbe653f0ea1
brianmhess <brianmhess@gmail.com>
2020-03-23 15:11:50 -0400

    a few things

--- src/main/java/hessian/example/SampleApplication.java
+++ src/main/java/hessian/example/SampleApplication.java
@@ -6,6 +6,8 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.*;
 import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
 import reactor.core.publisher.Flux;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jmx.JmxReporter;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.CompletionStage;
@@ -15,6 +17,8 @@ public class SampleApplication {
         final String dc = "dc1";
         final String ks = "ks";
         final String tbl = "tbl";
+        final int ddlRetries = 3;
+        final int ddlRetrySleepMs = 500;
         // Create CqlSession
         CqlSession session = CqlSession.builder()
 //                .withAuthCredentials("cass_user", "choose_a_better_password")  // redundant with application.conf, but showing for demonstration
@@ -22,14 +26,48 @@ public class SampleApplication {
                 .withLocalDatacenter("dc1")
                 .build();
 
-        // Create Keyspace and Table
+        // Set up driver metrics and expose via JMX (as an example)
+        MetricRegistry registry = session.getMetrics()
+                .orElseThrow(() -> new IllegalStateException("Metrics are disabled"))
+                .getRegistry();
+
+        JmxReporter reporter =
+                JmxReporter.forRegistry(registry)
+                        .inDomain("com.datastax.oss.driver")
+                        .build();
+        reporter.start();
+
+        // Create Keyspace
         SimpleStatement createKeyspace = SimpleStatement.newInstance("CREATE KEYSPACE IF NOT EXISTS "+ks
                 +" WITH replication = {'class': 'NetworkTopologyStrategy', '" + dc + "': '1'}");
         // Synchronous
-        session.execute(createKeyspace);
+        ResultSet ddl = null;
+        try {
+            ddl = session.execute(createKeyspace);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error creating keyspace");
+        }
+        if (!ddl.getExecutionInfo().isSchemaInAgreement()) {
+            int retries = 0;
+            while ((retries < ddlRetries) && session.checkSchemaAgreement()) {
+                try {
+                    Thread.sleep(ddlRetrySleepMs);
+                }
+                catch (InterruptedException ie) {
+                    throw new RuntimeException("Interrupted while waiting for schema agreement");
+                }
+                retries++;
+            }
+        }
+
+        // Create Table (using helper method)
         SimpleStatement createTable = SimpleStatement.newInstance("CREATE TABLE IF NOT EXISTS "+ks+"."+tbl
                 +"(pkey INT, x INT, PRIMARY KEY ((pkey)))");
-        session.execute(createTable);
+        if (!executeDdl(session, createTable, ddlRetries, ddlRetrySleepMs)) {
+            System.err.println("Error creating table");
+            System.exit(1);
+        }
 
         // Insert some data
         // Simple
@@ -38,6 +76,7 @@ public class SampleApplication {
         session.execute(stmt1);
 
         // Prepared
+        // Using positional bind markers
         SimpleStatement stmt2 = SimpleStatement.newInstance("INSERT INTO "+ks+"."+tbl+"(pkey,x) VALUES (?,?)");
         PreparedStatement prepared =  session.prepare(stmt2);
         BoundStatement bound1 = prepared.bind();
@@ -53,9 +92,13 @@ public class SampleApplication {
         }
 
         // Reactive
-        BoundStatement bound2 = prepared.bind();
-        bound2 = bound2.setInt(0, 100);
-        bound2 = bound2.unset(1); // redundant, but showing for demonstration
+        // Using named bind markers instead of positional
+        SimpleStatement stmt3 = SimpleStatement.newInstance("INSERT INTO "+ks+"."+tbl+"(pkey,x) VALUES (:pkey,:x)");
+        PreparedStatement prepared2 = session.prepare(stmt3);
+        BoundStatement bound2 = prepared2.bind();
+        bound2 = bound2.setInt("pkey", 100);
+        bound2 = bound2.setInt("x", 200);
+        bound2 = bound2.unset(1); // Showing for demonstration
         bound2 = bound2.setIdempotent(true); // set idempotency
         bound2 = bound2.setConsistencyLevel(ConsistencyLevel.LOCAL_ONE);
         ReactiveResultSet reactiveResult = session.executeReactive(bound2);
@@ -64,7 +107,7 @@ public class SampleApplication {
         // Batch
         BatchStatementBuilder batchBuilder = BatchStatement.builder(BatchType.UNLOGGED);
         BoundStatement bound3 = prepared.bind(1000,2000);
-        BoundStatement bound4 = prepared.bind(10000,20000);
+        BoundStatement bound4 = prepared2.bind().setInt("pkey",10000).setInt("x", 20000);
         batchBuilder.addStatement(bound3);
         batchBuilder.addStatement(bound4);
         BatchStatement batch = batchBuilder.build();
@@ -80,6 +123,28 @@ public class SampleApplication {
 
         // Cleanup
         session.close();
-        return;
+    }
+
+    private static boolean executeDdl(CqlSession session, Statement ddlStatement, int ddlRetries, int ddlRetrySleepMs) {
+        ResultSet ddl = null;
+        try {
+            ddl = session.execute(ddlStatement);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Exception while executing DDL (" + ddlStatement + ")");
+        }
+        if (!ddl.getExecutionInfo().isSchemaInAgreement()) {
+            int retries = 0;
+            while ((retries < ddlRetries) && session.checkSchemaAgreement()) {
+                try {
+                    Thread.sleep(ddlRetrySleepMs);
+                }
+                catch (InterruptedException ie) {
+                    throw new RuntimeException("Interrupted while waiting for schema agreement");
+                }
+                retries++;
+            }
+        }
+        return true;
     }
 }
